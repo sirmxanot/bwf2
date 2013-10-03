cbc6567979601f84017e1f8815cc72472ef050e5
Andrew Frank <andrewpaulfrank@gmail.com>
2013-10-03 16:40:45 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BillsWithFriends::Application.config.secret_token = '93b12e3df5a1b0410cc9545c289cb78247dd026182573d82ac948901918e6e54c8e8445411bc4702c5009d9f2235c3cf47ffbffe1d81a0392ea223839f9ebfa8'
